774c2ee5ac4de458059980d39899bb2cc5fc97fd
Shriyan Naidu <physicallychallenged560019@gmail.com>
2023-05-16 16:51:56 +0530

    first commit

--- src/components/Change_pw.tsx
+++ src/components/Change_pw.tsx
@@ -3,43 +3,39 @@ import { VariantProps, cva } from "class-variance-authority";
 export type ButtonProps = VariantProps<typeof buttonStyles>;
 
 export const buttonStyles = cva("px-2 py-2 flex items-center justify-center rounded-full", {
-  variants: {
-    intent: {
-      primary: 	"bg-gradient-to-t transition-all duration-75 font-medium hover:from-[#8CC6DA] hover:to-[#1389B2] hover:text-white",
+    variants: {
+        intent: {
+            primary: "bg-gradient-to-t transition-all duration-75 font-medium hover:from-[#8CC6DA] hover:to-[#1389B2] hover:text-white",
+        },
+        fullwidth: {
+            true: "w-full"
+        },
     },
-    fullwidth: {
-      true: "w-full"
+    defaultVariants: {
+        intent: "primary"
     },
-  },
-  defaultVariants: {
-    intent: "primary"
-  },
 });
 
 interface ButtonExtendedProps extends ButtonProps {
-  children: string;
-  LeftIcon?: React.ReactNode;
-  width?: string;
-  height?: string;
+    children: string;
+    width?: string;
+    height?: string;
 }
 
 export default function Button({
-  intent,
-  fullwidth,
-  children,
-  LeftIcon,
-  width,
-  height,
-  ...props
+    intent,
+    fullwidth,
+    children,
+    width,
+    height,
+    ...props
 }: ButtonExtendedProps) {
-  const buttonStyle = {
-    width : "338px" , height : "40px"
-  };
+    const buttonStyle = {
+        width: "338px",
+        height: "40px"
+    };
 
-  return (
-    <button className={buttonStyles({ intent, fullwidth })} style={buttonStyle} {...props}>
-      {LeftIcon && <div className="text-lg pr-3">{LeftIcon}</div>}
-      {children}
-    </button>
-  );
+    return (
+        <button className={buttonStyles({ intent, fullwidth })} style={buttonStyle} {...props}></button>
+    );
 }
